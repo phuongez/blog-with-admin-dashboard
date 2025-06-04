f2723d67443115fcaa0ea3e51fdcaed3b435968c
phuongez <duyphuong.hvnh@gmail.com>
2025-06-04 22:09:17 +0700

    Move pageskeleton to component

--- app/(home)/articles/page.tsx
+++ app/(home)/articles/page.tsx
@@ -6,6 +6,7 @@ import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { fetchArticleByQuery } from "@/lib/query/fetch-articles";
 import Link from "next/link";
+import AllArticlesPageSkeleton from "@/components/articles/AllArticlesPageSkeleton";
 
 type SearchPageProps = {
   searchParams: { search?: string; category?: string; page?: string };
@@ -102,29 +103,3 @@ const page: React.FC<SearchPageProps> = async ({ searchParams }) => {
 };
 
 export default page;
-
-export function AllArticlesPageSkeleton() {
-  return (
-    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      {Array.from({ length: 3 }).map((_, index) => (
-        <Card
-          key={index}
-          className="group relative overflow-hidden transition-all hover:shadow-lg"
-        >
-          <div className="p-6">
-            <Skeleton className="mb-4 h-48 w-full rounded-xl bg-gradient-to-br from-purple-100/50 to-blue-100/50 dark:from-purple-900/20 dark:to-blue-900/20" />
-            <Skeleton className="h-6 w-3/4 rounded-lg" />
-            <Skeleton className="mt-2 h-4 w-1/2 rounded-lg" />
-            <div className="mt-6 flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                <Skeleton className="h-8 w-8 rounded-full" />
-                <Skeleton className="h-4 w-20 rounded-lg " />
-              </div>
-              <Skeleton className="h-4 w-24 rounded-lg " />
-            </div>
-          </div>
-        </Card>
-      ))}
-    </div>
-  );
-}
