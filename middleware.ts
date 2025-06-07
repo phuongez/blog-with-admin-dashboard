1cb122099f61fdde951102e94a147410820ccc21
phuongez <duyphuong.hvnh@gmail.com>
2025-06-07 16:59:22 +0700

    Fix public route

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/sepay/webhook"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/sepay/webhook",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
