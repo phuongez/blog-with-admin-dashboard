d2f37a4433b96ef2d02e12245c88d44d1142f727
phuongez <duyphuong.hvnh@gmail.com>
2025-06-09 21:34:19 +0700

    Fix params

--- app/api/notifications/[id]/route.ts
+++ app/api/notifications/[id]/route.ts
@@ -1,10 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function PATCH(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PATCH(req: Request, { params }: any) {
   await prisma.notification.update({
     where: { id: params.id },
     data: { isRead: true },
