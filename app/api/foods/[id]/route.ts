7fe1e83f66dc6d57caa19c729cd571a322a56f95
phuongez <duyphuong.hvnh@gmail.com>
2025-06-12 10:52:29 +0700

    fix foods route params

--- app/api/foods/[id]/route.ts
+++ app/api/foods/[id]/route.ts
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(_: Request, { params }: { params: any }) {
   const { id } = params;
 
   if (!id) {
